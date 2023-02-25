39bdfecff3d1e5c6e495fab285b055d52077d27e
ciringa <78325414+ciringa@users.noreply.github.com>
2023-02-25 18:42:03 -0300

    Add files via upload

---
+++ pagina dashboard/code.js
@@ -0,0 +1,15 @@
+
+
+
+function goto(were){
+
+switch(were){
+case "twitter": window.open("https://twitter.com/Ciringa_Men"); break;
+case "itch.io": window.open("https://ciringa-men.itch.io"); break;
+case "youtube": window.open("https://www.youtube.com/channel/UCSgdkCEzfwy9T4yKZCOnSfQ"); break;
+
+
+}
+
+
+}
